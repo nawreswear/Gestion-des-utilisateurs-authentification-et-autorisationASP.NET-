8f4eaf7dda5d94fdd700d26499a8bfa180d2a384
nawres wear <nawreselou2382@gmail.com>
2025-10-01 20:33:22 +0200

    Ajoutez des fichiers projet.

---
+++ WebApplication1/Models/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models.Repositories
+{
+    public interface IProductRepository
+    {
+        Product GetById(int Id);
+        IList<Product> GetAll();
+        void Add(Product t);
+        Product Update(Product t);
+        void Delete(int Id);
+        public IList<Product> GetProductsByCategID(int? CategId);
+        public IList<Product> FindByName(string name);
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+using WebApplication1.Models.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Register DbContext
+builder.Services.AddDbContextPool<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ProductDBConnection"))
+);
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Default Password settings.
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+});
+// Register repositories
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+// Build the app
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
