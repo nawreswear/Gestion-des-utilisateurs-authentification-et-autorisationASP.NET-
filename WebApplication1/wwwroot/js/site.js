69df1f2f57c4e3728c2ce9438a28cbe184d38244
nawres wear <nawreselou2382@gmail.com>
2025-10-08 10:37:36 +0200

    cccc

--- WebApplication1/wwwroot/js/site.js
+++ WebApplication1/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener("DOMContentLoaded"
+    , function () {
+        var sidebar = document.getElementById("sidebar");
+        var mainContent = document.getElementById("mainContent");
+        var toggleButton = document.getElementById("toggleMenu");
+        // Gérer l'affichage du menu au clic
+        toggleButton.addEventListener("click", function () {
+            if (sidebar.style.display === "none") {
+                sidebar.style.display = "block";
+                mainContent.classList.remove("col-md-12");
+                mainContent.classList.add("col-md-10");
+            } else {
+                sidebar.style.display = "none";
+                mainContent.classList.remove("col-md-10");
+                mainContent.classList.add("col-md-12");
+            }
+        });
+    });
