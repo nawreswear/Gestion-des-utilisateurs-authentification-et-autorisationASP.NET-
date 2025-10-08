30f21a63614884a934012a919f1540868e9ef3b2
nawres wear <nawreselou2382@gmail.com>
2025-10-08 12:38:20 +0200

    qq

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -40,7 +40,7 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
-
+//app.UseSession();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
